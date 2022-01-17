0fbc5a7bf9859ff71df8294987a0a61beb717f88
tatsuoo000 <rush.life29@gmail.com>
2022-01-18 08:03:27 +0900

    feature init

--- dst/srcs/main.c
+++ dst/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tkano <tkano@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 07:41:24 by tkano             #+#    #+#             */
-/*   Updated: 2022/01/11 17:55:48 by tkano            ###   ########.fr       */
+/*   Updated: 2022/01/14 22:31:29 by tkano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 int	main(int argc, char **argv)
 {
 	t_info	info;
-	t_philo	*philos;
+	t_philo	*philos = NULL;
 
 	if (!arg_parse(&info, argc, argv))
 		return (exit_free(NULL, NULL, "Invalid arguments."));
 	if (info_init(&info))
 		return (exit_free(&info, NULL, "failed to init infos."));
+/*
 	if (philos_init(&info, philos))
 		return (exit_free(&info, philos, "failed to init philos."));
 	start(philos);
+*/
 	return (exit_free(&info, philos, NULL));
 }
